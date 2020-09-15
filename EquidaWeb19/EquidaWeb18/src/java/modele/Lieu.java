9fece239f514bc7389c8d04e1e547f7478773d17
muthsM <melvynmuths@gmaiL.com>
2020-09-15 11:09:18 +0200

    ajout des fichier du projet decompressé

---
+++ EquidaWeb19/EquidaWeb18/src/java/modele/Lieu.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+/**
+ *
+ * @author Zakina
+ */
+public class Lieu {
+    private int id ;
+
+    public Lieu(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+    
+}
